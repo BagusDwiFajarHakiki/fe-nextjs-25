99c856d7dee718a2faac3ba19705a51ee62cae13
BagusDwi <bagusdwifajarhakiki18@gmail.com>
2025-12-23 10:25:29 +0700

    feat: add product category page with basic layout and data fetching.

--- app/product-category/page.tsx
+++ app/product-category/page.tsx
@@ -1,6 +1,17 @@
 import Layout from "@/components/ui/Layout";
+import { service } from '@/services/services';
+import React, { useEffect } from 'react';
 
 export default function Page() {
+  const getData = async () => {
+    const response = await service('product-category');
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Layout>
       <h1 className="text-black">
