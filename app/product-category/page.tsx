798bc60161366fad4d5f909b0fe8cc4b50dacdfd
BagusDwi <bagusdwifajarhakiki18@gmail.com>
2025-12-30 10:40:20 +0700

    feat: Implement product category page with basic layout, API integration, and MUI DataGrid.

--- app/product-category/page.tsx
+++ app/product-category/page.tsx
@@ -1,12 +1,28 @@
-"use client"
+"use client";
 
 import Layout from "@/components/ui/Layout";
-import { service } from '@/services/services';
-import React, { useEffect } from 'react';
+import { service } from "@/services/services";
+import React, { useEffect, useState } from "react";
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+
+const rows: GridRowsProp = [
+  { id: 1, name: "Data Grid", description: "the Community version" },
+  { id: 2, name: "Data Grid Pro", description: "the Pro version" },
+  { id: 3, name: "Data Grid Premium", description: "the Premium version" },
+];
+
+const columns: GridColDef[] = [
+  { field: "name", headerName: "Product Name", width: 200 },
+  { field: "description", headerName: "Description", width: 300 },
+];
 
 export default function Page() {
+  const [rows, setRows] = useState<any>([]);
   const getData = async () => {
-    const response = await service('categories');
+    const response = await service("categories");
+    if (!response.error) {
+      setRows(response.data);
+    }
     console.log(response);
   };
 
@@ -16,9 +32,10 @@ export default function Page() {
 
   return (
     <Layout>
-      <h1 className="text-black">
-        Product Category
-      </h1>
+      <h1 className="text-black">Product Category</h1>
+      <div style={{ height: 300, width: "100%" }}>
+        <DataGrid rows={rows} columns={columns} />
+      </div>
     </Layout>
-  )
+  );
 }
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-<nav className="w-full relative bg-gray-800">
+<nav className="rounded-md shadow-lg/30 w-full relative bg-gray-800">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div className="relative flex h-16 items-center justify-between">
       <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
