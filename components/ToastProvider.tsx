07d4c151c9067d1eac7533eec47c908e790efa39
BagusDwi <bagusdwifajarhakiki18@gmail.com>
2026-01-20 01:26:42 +0700

    feat: Implement full CRUD operations for product categories, including dedicated pages, data types, and API integration.

---
+++ components/ToastProvider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import "react-toastify/dist/ReactToastify.css";
+import React from "react";
+import { ToastContainer } from "react-toastify";
+
+export default function ToastProvider() {
+  return <ToastContainer />;
+}
